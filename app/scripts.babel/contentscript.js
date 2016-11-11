1c1e742d5c8ebbbd31596a44f7acbe9ca25e5528
Stefan Kendall <stefankendall@gmail.com>
2016-11-10 21:04:22 -0500

    Connect popup and page.

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,1 +1,14 @@
 'use strict';
+
+class PageManipulator {
+  randomizeRadioInputs() {
+    console.log('randomizin');
+  }
+}
+
+chrome.runtime.onMessage.addListener(
+  function (request, sender) {
+    if (request.action === 'randomizeRadioEntries') {
+      new PageManipulator().randomizeRadioInputs();
+    }
+  });
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+const activeTab = function () {
+  return new Promise(function (resolve) {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+      resolve(tabs[0]);
+    });
+  });
+};
+
+window.randomizeRadioEntries = function () {
+  activeTab().then((tab) => {
+    chrome.tabs.sendMessage(tab.id, {action: 'randomizeRadioEntries'});
+  });
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('radio-button').addEventListener('click', randomizeRadioEntries);
+});
