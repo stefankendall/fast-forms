562bb81378f09c7a475fc5579e00ac9b8be5f6c8
Stefan Kendall <stefankendall@gmail.com>
2016-11-10 20:30:25 -0500

    Add jquery/lodash

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,9 +1,6 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(details => {
-  console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
-
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+chrome.browserAction.setBadgeText({text: 'fast'});
